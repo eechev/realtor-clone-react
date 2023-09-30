b2afb9fab9a0c7399ee6108dd0774ada72c5ade1
Elgardo Echevarria <elgardoe@hotmail.com>
2023-09-30 14:04:48 -0400

    fix issue when header was not properly updating borders

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,22 +24,34 @@ export default function Header() {
         <div>
           <ul className='flex space-x-10'>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-              ${pathMatchRoute('/') && 'text-black border-b-red-500'}`}
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] 
+              ${
+                pathMatchRoute('/')
+                  ? 'text-black border-b-red-500'
+                  : ' text-gray-400 border-b-transparent'
+              }`}
               onClick={() => navigate('/')}
             >
               Home
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-              ${pathMatchRoute('/offers') && 'text-black border-b-red-500'}`}
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px] 
+              ${
+                pathMatchRoute('/offers')
+                  ? 'text-black border-b-red-500'
+                  : ' text-gray-400 border-b-transparent'
+              }`}
               onClick={() => navigate('/offers')}
             >
               Offers
             </li>
             <li
-              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent 
-              ${pathMatchRoute('/sign-in') && 'text-black border-b-red-500'}`}
+              className={`cursor-pointer py-3 text-sm font-semibold border-b-[3px]
+              ${
+                pathMatchRoute('/sign-in')
+                  ? 'text-black border-b-red-500'
+                  : ' text-gray-400 border-b-transparent'
+              }`}
               onClick={() => navigate('/sign-in')}
             >
               Sign in
